04ed49e3312595c14b0cf515111bdbe9a3a9b3dd
Jonathan Schroeter <jonny.schroeter@gmail.com>
2021-09-25 15:49:05 -0500

    Update settings.rs

--- packages/datajoint-core/src/connection/settings.rs
+++ packages/datajoint-core/src/connection/settings.rs
@@ -25,15 +25,15 @@ impl ConnectionSettings{
             use_tls: None,
         }
     }
-}
-
-fn createuri() -> String{
-    let urimaker = ConnectionSettings::new();
-    let mut uri = "".to_string();
-    if urimaker.database_type == DatabaseType::MySql {
-        uri = format!("mysql://{}:{}@{}:{}/{}",urimaker.username,urimaker.password,urimaker.hostname,urimaker.port.to_string(),urimaker.database_name);
-    }else if urimaker.database_type == DatabaseType::Postgres {
-        uri = format!("postgres://{}:{}@{}:{}/{}",urimaker.username,urimaker.password,urimaker.hostname,urimaker.port.to_string(),urimaker.database_name);
-    } 
-    return uri;
+    fn uri() -> String{
+        let urimaker = ConnectionSettings::new();
+        let mut uri = "".to_string();
+        if urimaker.database_type == DatabaseType::MySql {
+            uri = format!("mysql://{}:{}@{}:{}/{}",urimaker.username,urimaker.password,urimaker.hostname,urimaker.port.to_string(),urimaker.database_name);
+        }else if urimaker.database_type == DatabaseType::Postgres {
+            uri = format!("postgres://{}:{}@{}:{}/{}",urimaker.username,urimaker.password,urimaker.hostname,urimaker.port.to_string(),urimaker.database_name);
+        } 
+        println!("{}", uri);
+        return uri;
+    }
 }
