9b2a21a52391b374d14e023e0d1e7bb9ccc13586
Jonathan Schroeter <jas170005@utdallas.edu>
2021-09-24 20:07:26 -0500

    Default Settings, and URI creator

---
+++ packages/datajoint-core/src/connection/settings.rs
@@ -0,0 +1,40 @@
+#[derive(Eq, PartialEq)]
+enum DatabaseType {
+    MySql,
+    Postgres,
+}
+struct ConnectionSettings {
+    database_type: DatabaseType,
+    username: String,
+    password: String,
+    hostname: String,
+    port: u16,
+    database_name: String,
+    use_tls: Option<bool>,
+}
+
+impl ConnectionSettings{
+    fn new() -> ConnectionSettings{
+        ConnectionSettings{
+            database_type: DatabaseType::MySql,
+            username: "".to_string(),
+            password: "".to_string(),
+            hostname: "localhost".to_string(),
+            port: 3306,
+            database_name: "".to_string(),
+            use_tls: None,
+        }
+    }
+}
+
+fn createuri() -> String{
+    let urimaker = ConnectionSettings::new();
+    let mut uri = "".to_string();
+    if urimaker.database_type == DatabaseType::MySql {
+        uri = format!("mysql://{}:{}@{}:{}/{}",urimaker.username,urimaker.password,urimaker.hostname,urimaker.port.to_string(),urimaker.database_name);
+    }else if urimaker.database_type == DatabaseType::Postgres {
+        uri = format!("postgres://{}:{}@{}:{}/{}",urimaker.username,urimaker.password,urimaker.hostname,urimaker.port.to_string(),urimaker.database_name);
+    } 
+    println!("{}", uri);
+    return uri;
+}
