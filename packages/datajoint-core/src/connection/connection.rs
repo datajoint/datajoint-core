d151c887c19d83ab0c6347faf7d2b552c291b7db
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2021-09-28 19:59:33 -0500

    Move connection class and implement cursors.

---
+++ packages/datajoint-core/src/connection/connection.rs
@@ -0,0 +1,99 @@
+use crate::connection::cursor::Cursor;
+
+/// A single connection instance to an arbitrary SQL database.
+pub struct Connection {
+    settings: String,
+    pool: Option<sqlx::AnyPool>,
+    runtime: tokio::runtime::Runtime,
+}
+
+impl Connection {
+    /// Creates a new connection to a SQL database based on the given settings.
+    ///
+    /// The connection is not actuall established until [.connect()][Connection::connect]
+    /// is called.
+    pub fn new(settings: String) -> Self {
+        Connection {
+            settings: settings,
+            pool: None,
+            runtime: tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .ok()
+                .unwrap(),
+        }
+    }
+
+    /// Starts the connection to the SQL database according to settings the object was
+    /// initialized with.
+    pub fn connect(&mut self) -> Result<(), &str> {
+        self.pool = Some(Connection::get_pool(&self.runtime, &*self.settings)?);
+        return Ok(());
+    }
+
+    /// Disconnects from the SQL database.
+    ///
+    /// The connection can be restarted if desired.
+    pub fn disconnect(&mut self) -> Result<(), &str> {
+        // TODO(jnestelroad): Implement with self.pool.close() async.
+        self.pool = None;
+        return Ok(());
+    }
+
+    fn get_pool(
+        runtime: &tokio::runtime::Runtime,
+        uri: &str,
+    ) -> Result<sqlx::AnyPool, &'static str> {
+        runtime.block_on(Connection::get_pool_async(uri))
+    }
+
+    async fn get_pool_async(uri: &str) -> Result<sqlx::AnyPool, &'static str> {
+        match sqlx::any::AnyPoolOptions::new()
+            // TODO(jnestelroad): Allow more than one connection in settings?
+            .max_connections(1)
+            .connect(uri)
+            .await
+        {
+            Err(_) => Err("failed to get_pool_async"),
+            Ok(pool) => Ok(pool),
+        }
+    }
+
+    /// Creates a cursor to interact with the database over this connection.
+    ///
+    /// Panics on error.
+    pub fn cursor<'c>(&'c self) -> Cursor<'c> {
+        self.try_cursor().unwrap()
+    }
+
+    /// Creates a cursor to interact with the database over this connection.
+    pub fn try_cursor<'c>(&'c self) -> Result<Cursor<'c>, &str> {
+        match &self.pool {
+            None => Err("error in cursor"),
+            Some(pool) => Ok(Cursor::new(pool, &self.runtime)),
+        }
+    }
+
+    /// Creates a cursor for a raw query, given as a string, to execute over the connection.
+    ///
+    /// Panics on error.
+    pub fn raw_query<'c>(&'c self, query: &'c str) -> Cursor<'c> {
+        self.try_raw_query(query).unwrap()
+    }
+
+    // Creates a cursor for a raw query, given as a string, to execute over the connection.
+    pub fn try_raw_query<'c>(&'c self, query: &'c str) -> Result<Cursor<'c>, &str> {
+        self.runtime.block_on(self.raw_query_async(query))
+    }
+
+    async fn raw_query_async<'c>(&'c self, query: &'c str) -> Result<Cursor<'c>, &str> {
+        match &self.pool {
+            None => Err("error in query_async"),
+            Some(pool) => {
+                let mut cursor = Cursor::new(pool, &self.runtime);
+                cursor.execute(query);
+                Ok(cursor)
+            }
+        }
+    }
+}
--- packages/datajoint-core/src/connection/mod.rs
+++ packages/datajoint-core/src/connection/mod.rs
@@ -1,42 +1,5 @@
-// use sqlx::postgres::PgPoolOptions;
-// use sqlx::mysql::MySqlPoolOptions;
+mod connection;
+mod cursor;
 
-pub struct Connection {
-    host: String,
-    user: String,
-    password: String,
-    reset: bool,
-    use_tls: bool
-}
-
-impl Connection {
-    pub fn new(host: &str, user: &str, password: &str, reset: bool, use_tls: bool) -> Self {
-        Connection {
-            host: host.to_string(),
-            user: user.to_string(),
-            password: password.to_string(),
-            reset,
-            use_tls,
-        }
-    }
-
-    // Support for MySQL and Postgres database servers
-    // No dynamic database configuration (hard-coded into the code for now perhaps at compile time)
-    pub fn connect(&mut self) {
-        println!("Connected to database with the following settings:\n\
-                Host: {}\n\
-                User: {}\n\
-                Password: {}\n\
-                Reset: {}\n\
-                use_TLS: {} ",
-                self.host, self.user, self.password, self.reset, self.use_tls);
-    }
-
-    // Provide a utility in the lib to receive a Generic SQL query and
-    //      execute against a relational database server
-    // No placeholder arguments (hard-coded into the queries for now)
-    pub fn raw_query(&self, query: &str) -> u32 {
-        println!("Making query from rust library: {}", query);
-        0
-    }
-}
+pub use connection::Connection;
+pub use cursor::Cursor;
