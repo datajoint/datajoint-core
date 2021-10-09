1c1a8caffc50ce89accb49932447bad4129d3cc9
Kaerah <kaerah.lopez@gmail.com>
2021-10-08 22:56:58 -0500

    Add more table-row-get functions

--- packages/datajoint-core-ffi-c/src/results/table_row.rs
+++ packages/datajoint-core-ffi-c/src/results/table_row.rs
@@ -4,6 +4,7 @@ use datajoint_core::results::TableRow;
 use libc::c_char;
 use std::ffi::{CString, CStr};
 
+/// TableRow C FFI
 #[no_mangle]
 pub extern "C" fn table_row_free(ptr: *mut TableRow) {
     if ptr.is_null() {
@@ -78,6 +79,60 @@ pub extern "C" fn table_row_get_date(ptr: *const TableRow, column_name: *const c
 }
 
 #[no_mangle] 
+pub extern "C" fn table_row_get_tinyint(ptr: *const TableRow, column_name: *const c_char, mut _out: *mut c_char) -> i8 {
+    let row: &TableRow = unsafe {
+        if ptr.is_null() {
+            return -1;
+        }
+        &*ptr
+    };
+
+    let col_name = unsafe {
+        if column_name.is_null() {
+            return -1;
+        }
+        CStr::from_ptr(column_name)
+    };
+
+    let col_name = col_name.to_str();
+    let col_name : &str = match col_name {
+        Ok(name) => name,
+        Err(_) => return -1
+    };
+
+    let data: &str = row.get(col_name);
+    _out = CString::new(data).unwrap().into_raw();
+    0
+}
+
+#[no_mangle] 
+pub extern "C" fn table_row_get_tinyint_unsigned(ptr: *const TableRow, column_name: *const c_char, mut _out: *mut c_char) -> i8 {
+    let row: &TableRow = unsafe {
+        if ptr.is_null() {
+            return -1;
+        }
+        &*ptr
+    };
+
+    let col_name = unsafe {
+        if column_name.is_null() {
+            return -1;
+        }
+        CStr::from_ptr(column_name)
+    };
+
+    let col_name = col_name.to_str();
+    let col_name : &str = match col_name {
+        Ok(name) => name,
+        Err(_) => return -1
+    };
+
+    let data: &str  = row.get(col_name);
+    _out = CString::new(data).unwrap().into_raw();
+    0
+}
+
+#[no_mangle] 
 pub extern "C" fn table_row_get_ordinal_char_n(ptr: *const TableRow, column: usize , mut _out: *mut c_char) -> i8 {
     let row: &TableRow = unsafe {
         if ptr.is_null() {
@@ -86,11 +141,10 @@ pub extern "C" fn table_row_get_ordinal_char_n(ptr: *const TableRow, column: usi
         &*ptr
     };
 
-    let res_str: &str = row.get(column);
-    _out = CString::new(res_str).unwrap().into_raw();
+    let data : &str = row.get(column);
+    _out = CString::new(data).unwrap().into_raw();
     0
 }
-
 
 #[no_mangle] 
 pub extern "C" fn table_row_get_ordinal_date(ptr: *const TableRow, column: usize , mut _out: *mut c_char) -> i8 {
@@ -101,7 +155,35 @@ pub extern "C" fn table_row_get_ordinal_date(ptr: *const TableRow, column: usize
         &*ptr
     };
 
-    let res_str: &str = row.get(column);
-    _out = CString::new(res_str).unwrap().into_raw();
+    let data: &str = row.get(column);
+    _out = CString::new(data).unwrap().into_raw();
+    0
+}
+
+#[no_mangle] 
+pub extern "C" fn table_row_get_ordinal_tinyint(ptr: *const TableRow, column: usize , mut _out: *mut c_char) -> i8 {
+    let row: &TableRow = unsafe {
+        if ptr.is_null() {
+            return -1;
+        }
+        &*ptr
+    };
+
+    let data: &str = row.get(column);
+    _out = CString::new(data).unwrap().into_raw();
+    0
+}
+
+#[no_mangle] 
+pub extern "C" fn table_row_get_ordinal_tinyint_unsigned(ptr: *const TableRow, column: usize , mut _out: *mut c_char) -> i8 {
+    let row: &TableRow = unsafe {
+        if ptr.is_null() {
+            return -1;
+        }
+        &*ptr
+    };
+
+    let data: &str = row.get(column);
+    _out = CString::new(data).unwrap().into_raw();
     0
 }
