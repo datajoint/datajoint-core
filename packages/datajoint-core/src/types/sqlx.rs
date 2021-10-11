6741dc3435b666de790ddfdd2eccc009283b93d4
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2021-10-10 23:55:59 -0500

    More detailed comment for timestamp issue.

--- packages/datajoint-core/src/types/sqlx.rs
+++ packages/datajoint-core/src/types/sqlx.rs
@@ -23,7 +23,11 @@ impl DataJointType {
             "TIME" => Time,
             "DATETIME" => DateTime,
             // MySQL considers timestamps to have a timezone, Postgres does not.
-            // This is a major problem.
+            // This is a major problem that currently cannot be resolved without
+            // providing the database type stored in ConnectionSettings to
+            // TableColumnRef::from_sqlx_type_name.
+            //
+            // For now, Timestamp is broken in MySQL.
             "TIMESTAMP" => Timestamp,
             "CHAR" => CharN,
             "VARCHAR" => VarCharN,
