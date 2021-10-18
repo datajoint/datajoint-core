067635c3c00a2f0f37650e85eddf57c55ada3f97
Jonathan <cs6alto@verizon.net>
2021-10-18 13:40:01 -0500

    fix: implemented __setitem__ and __getitem__ instead of update

--- datajoint-python/datajoint/connection.py
+++ datajoint-python/datajoint/connection.py
@@ -1,49 +1,55 @@
+from .datajoint_core_lib import dj_core
 from ._datajoint_core import ffi
-from .cffi_config import library_file
-C = ffi.dlopen(library_file)
 
 from .settings import config
-from .errors import check_error
+from .errors import datajoint_core_assert_success
 
 class Connection:
     def __init__(self, config):
-        self._conn = C.connection_new(config)
+        self._conn = dj_core.connection_new(config)
         self.connect()
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        C.connection_free(self._conn)
+        dj_core.connection_free(self._conn)
 
     def connect(self):
         print("Attempting to make connection")
-        err = C.connection_connect(self._conn)
-        check_error(err)
+        err = dj_core.connection_connect(self._conn)
+        datajoint_core_assert_success(err)
 
     def disconnect(self):
-        err = C.connectin_disconnect(self._conn)
-        check_error(err)
+        err = dj_core.connection_disconnect(self._conn)
+        datajoint_core_assert_success(err)
 
     def reconnect(self):
-        self.disconnect()
-        self.connect()
+        err = dj_core.connection_reconnect(self._conn)
+        datajoint_core_assert_success(err)
 
     def execute_query(self, query):
         out = ffi.new("uint64_t *")
-        err = C.connection_execute_query(self._conn, query.encode('utf-8'), out)
-        check_error(err)
-        out = out[0]
-        print(f'rows found: {out}')
+        err = dj_core.connection_execute_query(self._conn, query.encode('utf-8'), out)
+        datajoint_core_assert_success(err)
+        return out[0]
+
+    def fetch_query(self, query):
+        pass
+        # out = Cursor()
+        # err = dj_core.connection_fetch_query(self._conn, query.encode('utf-8'), out)
+        # datajoint_core_assert_success(err)
+        # return out
+
 
 def conn(host=None, user=None, password=None, database_name=None, *, init_fun=None, reset=False, use_tls=None):
     if host is not None:
-        config.update("hostname", host)
+        config["hostname"] = host
     if user is not None:
-        config.update("username", user)
+        config["username"] = user
     if password is not None:
-        config.update("password", password)
+        config["password"] = password
     if database_name is not None:
-        config.update("database_name", database_name")
+        config["database_name"] = database_name
     conn.Connection = Connection(config._config)
     return conn.Connection
