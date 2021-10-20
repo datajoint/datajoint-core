bbfb4dea04a3e7be0bc0bb09d9fe22a62a84cb22
Kaerah <kaerah.lopez@gmail.com>
2021-10-19 20:27:43 -0500

    Use TableColumnRef class in TableRow

--- datajoint-python/datajoint/table_row.py
+++ datajoint-python/datajoint/table_row.py
@@ -1,28 +1,29 @@
 from ._datajoint_core import ffi
 from .datajoint_core_lib import dj_core
 from .errors import datajoint_core_assert_success
+from .table_column_ref import TableColumnRef
 class TableRow:
     """
     TableRow class
     """
 
     def __init__(self):
-        self._table_rows = dj_core.table_row_new()
+        self.native = dj_core.table_row_new()
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        dj_core.table_row_free(self._table_rows)
+        dj_core.table_row_free(self.native)
 
     def __del__(self):
-        dj_core.table_row_free(self._table_rows)
+        dj_core.table_row_free(self.native)
 
     def is_empty(self):
         """
         Check if TableRow is empty
         """
-        res = dj_core.table_row_is_empty(self._table_rows)
+        res = dj_core.table_row_is_empty(self.native)
         datajoint_core_assert_success(res)
         return bool(res)
 
@@ -30,21 +31,26 @@ class TableRow:
         """
         Get all TableColumns
         """
-        out_columns = ffi.new("TableColumnRef*")
-        columns_size = ffi.new("int*")
+        out_columns = ffi.new("TableColumnRef**")
+        columns_size = ffi.new("size_t*")
         try:
-            status = dj_core.table_row_columns(self._table_rows, out_columns , columns_size)
+            status = dj_core.table_row_columns(self.native, out_columns, columns_size)
+            datajoint_core_assert_success(status)
+            output = []
+            for i in range(0, columns_size[0]):
+            # Allows TableColumnRef Python wrapper to take ownership of the next element.
+            # A little bit unsure of this dereferencing here, need to test.
+                output.append(TableColumnRef(out_columns[i]))
+                out_columns[i] = ffi.NULL
+            return output
         finally:
             dj_core.table_row_columns_free(out_columns, columns_size)
-        datajoint_core_assert_success(status)
-
-        return [out_columns[i] for i in range(0, columns_size[0])]
 
     def column_count(self):
         """
         Get number of columns in TableRow
         """
-        return dj_core.table_row_column_count(self._table_rows)
+        return dj_core.table_row_column_count(self.native)
 
     def column(self, index):
         """
@@ -52,6 +58,6 @@ class TableRow:
         """
         out_column = ffi.new("TableColumnRef*")
         # check if index is a ordinal or name
-        status = dj_core.table_row_get_column_with_name(self._table_rows, index.encode('utf-8'), out_column) if isinstance(index, str) else dj_core.table_row_get_column_with_ordinal(self._table_rows, index, out_column)
+        status = dj_core.table_row_get_column_with_name(self.native, index.encode('utf-8'), out_column) if isinstance(index, str) else dj_core.table_row_get_column_with_ordinal(self.native, index, out_column)
         datajoint_core_assert_success(status)
-        return out_column[0]
+        return TableColumnRef(out_column)
