398e832e3ff44bce48630432f473b43767463208
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2021-11-08 15:14:22 -0600

    Update code and fix lifetime issue.

--- packages/datajoint-core-ffi-c/src/connection/executor.rs
+++ packages/datajoint-core-ffi-c/src/connection/executor.rs
@@ -1,9 +1,11 @@
+use crate::error::datajoint_core_set_last_error;
 use crate::results::table_row_vector::TableRowVector;
 use crate::util;
 use datajoint_core::connection::{Cursor, Executor};
-use datajoint_core::error::ErrorCode;
+use datajoint_core::error::{DataJointError, ErrorCode};
+use datajoint_core::placeholders::PlaceholderArgumentVector;
 use datajoint_core::results::TableRow;
-use libc::{c_char, c_void, free, malloc, size_t};
+use libc::c_char;
 use std::ffi::CStr;
 
 #[no_mangle]
@@ -20,15 +22,19 @@ pub unsafe extern "C" fn executor_execute(
     out_size: *mut u64,
 ) -> i32 {
     if this.is_null() || query.is_null() {
-        return ErrorCode::NullNotAllowed as i32;
+        return datajoint_core_set_last_error(DataJointError::new(ErrorCode::NullNotAllowed))
+            as i32;
     }
     let executor = { &mut *this };
     let query_str = match CStr::from_ptr(query).to_str() {
-        Err(_) => return ErrorCode::InvalidCString as i32,
+        Err(_) => {
+            return datajoint_core_set_last_error(DataJointError::new(ErrorCode::InvalidUtf8String))
+                as i32
+        }
         Ok(value) => value,
     };
     match executor.try_execute(&query_str.to_string()) {
-        Err(error) => error.code() as i32,
+        Err(error) => datajoint_core_set_last_error(error) as i32,
         Ok(value) => {
             if !out_size.is_null() {
                 *out_size = value;
@@ -45,15 +51,19 @@ pub unsafe extern "C" fn executor_fetch_one(
     out: *mut *mut TableRow,
 ) -> i32 {
     if this.is_null() {
-        return ErrorCode::NullNotAllowed as i32;
+        return datajoint_core_set_last_error(DataJointError::new(ErrorCode::NullNotAllowed))
+            as i32;
     }
     let executor = { &mut *this };
     let query_str = match CStr::from_ptr(query).to_str() {
-        Err(_) => return ErrorCode::InvalidCString as i32,
+        Err(_) => {
+            return datajoint_core_set_last_error(DataJointError::new(ErrorCode::InvalidUtf8String))
+                as i32
+        }
         Ok(value) => value,
     };
     match executor.try_fetch_one(&query_str.to_string()) {
-        Err(error) => error.code() as i32,
+        Err(error) => datajoint_core_set_last_error(error) as i32,
         Ok(row) => {
             util::mem::handle_output_ptr(out, row);
             ErrorCode::Success as i32
@@ -68,15 +78,19 @@ pub unsafe extern "C" fn executor_fetch_all(
     out: *mut *mut TableRowVector,
 ) -> i32 {
     if this.is_null() {
-        return ErrorCode::NullNotAllowed as i32;
+        return datajoint_core_set_last_error(DataJointError::new(ErrorCode::NullNotAllowed))
+            as i32;
     }
     let executor = { &mut *this };
     let query_str = match CStr::from_ptr(query).to_str() {
-        Err(_) => return ErrorCode::InvalidCString as i32,
+        Err(_) => {
+            return datajoint_core_set_last_error(DataJointError::new(ErrorCode::InvalidUtf8String))
+                as i32
+        }
         Ok(value) => value,
     };
     match executor.try_fetch_all(&query_str.to_string()) {
-        Err(error) => error.code() as i32,
+        Err(error) => datajoint_core_set_last_error(error) as i32,
         Ok(rows) => {
             util::mem::handle_output_ptr(out, TableRowVector::new(rows));
             ErrorCode::Success as i32
@@ -85,19 +99,33 @@ pub unsafe extern "C" fn executor_fetch_all(
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn executor_cursor(
-    this: *mut Executor,
+pub unsafe extern "C" fn executor_cursor<'c>(
+    this: *mut Executor<'c>,
     query: *const c_char,
-    out: *mut *mut Cursor,
+    args: *mut PlaceholderArgumentVector,
+    out: *mut *mut Cursor<'c>,
 ) -> i32 {
     if this.is_null() {
-        return ErrorCode::NullNotAllowed as i32;
+        return datajoint_core_set_last_error(DataJointError::new(ErrorCode::NullNotAllowed))
+            as i32;
     }
     let executor = { &*this };
     let query_str = match CStr::from_ptr(query).to_str() {
-        Err(_) => return ErrorCode::InvalidCString as i32,
+        Err(_) => {
+            return datajoint_core_set_last_error(DataJointError::new(ErrorCode::InvalidUtf8String))
+                as i32
+        }
         Ok(value) => value,
     };
-    util::mem::handle_output_ptr(out, executor.cursor(query_str)); // TODO(jonathan-hocevar): FIX LIFETIME ERROR
-    ErrorCode::Success as i32
+    match if args.is_null() {
+        executor.cursor(query_str)
+    } else {
+        executor.cursor_ph(query_str, *Box::from_raw(args))
+    } {
+        Err(error) => datajoint_core_set_last_error(error) as i32,
+        Ok(cursor) => {
+            util::mem::handle_output_ptr(out, cursor);
+            ErrorCode::Success as i32
+        }
+    }
 }
