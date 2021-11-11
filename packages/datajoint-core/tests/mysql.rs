328019305b3d9dbae91325793ebb194b3392aec8
EdwardGarmon <38167308+EdwardGarmon@users.noreply.github.com>
2021-11-11 14:46:02 -0600

    moved testing to compose file

--- packages/datajoint-core/tests/mysql.rs
+++ packages/datajoint-core/tests/mysql.rs
@@ -14,9 +14,9 @@ fn test_connection_to_db() {
     settings.password = "password".to_string();
     settings.database_name = "datajoint_core".to_string();
     settings.use_tls = Some(true);
-    settings.hostname = "localhost".to_string();
-
+    settings.hostname = "mysql_5_7".to_string();
     let mut conn = Connection::new(settings);
+
     let result = conn.connect();
     assert!(result.is_ok(), "Connection did not connect.");
     let connected = conn.is_connected();
@@ -39,7 +39,7 @@ fn test_insert_and_retrieve_one_row() {
     settings.password = "password".to_string();
     settings.database_name = "datajoint_core".to_string();
     settings.use_tls = Some(true);
-    settings.hostname = "localhost".to_string();
+    settings.hostname = "mysql_5_7".to_string();
     let mut con = Connection::new(settings);
 
     con.connect().unwrap();
@@ -49,30 +49,36 @@ fn test_insert_and_retrieve_one_row() {
     let cursor = &mut con.fetch_query("select text, owner_id from tweet");
     let cursor = cursor;
 
-    let row: TableRow = cursor.next();
-    let cols = row.columns();
+    let rows: Vec<TableRow> = cursor.rest();
+    let cols = rows[0].columns();
+    if rows[0].is_empty() {
+        assert!(false, "Failed to read one row.");
+    }
 
-    let text = match row.try_decode(cols[0]) {
+    let col_count = rows[0].column_count();
+    assert!(col_count == 2, "Column count did not equal 2");
+
+    let cols = rows[0].columns();
+
+    let text = match rows[0].try_decode(cols[0]) {
         Ok(data) => data,
         Err(_) => NativeType::None,
     };
 
-    let owner_id = match row.try_decode(cols[1]) {
+    let owner_id = match rows[0].try_decode(cols[1]) {
         Ok(data) => data,
         Err(_) => NativeType::None,
     };
 
-    println!("{}", text);
-
-    //TODO(EdwardGarmon): add these asserts back once we get everything running
-    // assert!(
-    //     text == NativeType::String("hello world".to_string()),
-    //     "text did not match \"hello world\"."
-    // );
-    // assert!(
-    //     owner_id == NativeType::Int64(1234),
-    //     "owner_id did not equal 1234."
-    // );
+    assert!(
+        text == NativeType::String("hello world".to_string()),
+        "text did not match \"hello world\"."
+    );
+    assert!(
+        owner_id == NativeType::Int64(1234),
+        "owner_id did not equal 1234."
+    );
+    con.disconnect();
 }
 
 #[test]
@@ -84,7 +90,7 @@ fn test_insert_and_retrieve_multiple_rows() {
     settings.password = "password".to_string();
     settings.database_name = "datajoint_core".to_string();
     settings.use_tls = Some(true);
-    settings.hostname = "localhost".to_string();
+    settings.hostname = "mysql_5_7".to_string();
     let mut con = Connection::new(settings);
 
     con.connect().unwrap();
@@ -112,7 +118,43 @@ fn test_insert_and_retrieve_multiple_rows() {
             }
         }
     }
+    con.disconnect()
+}
+
+#[test]
+fn test_placeholders() {
+    let mut settings = ConnectionSettings::new();
+    settings.database_type = DatabaseType::MySql;
+    settings.username = "root".to_string();
+    settings.port = 3306;
+    settings.password = "password".to_string();
+    settings.database_name = "datajoint_core".to_string();
+    settings.use_tls = Some(true);
+    settings.hostname = "mysql_5_7".to_string();
+    let mut con = Connection::new(settings);
+
+    con.connect().unwrap();
     con.execute_query("truncate tweet");
+    con.execute_query("insert into tweet (text, owner_id) values ('hello world', 1234);");
+
+    let placeholders: Vec<NativeType> = vec![NativeType::Int32(1234)];
+    let mut cursor = con.fetch_query_ph("select * from tweet where owner_id = ?;", placeholders);
+    let rows: Vec<TableRow> = cursor.rest();
+
+    for row in rows {
+        for col in row.columns() {
+            match row.try_decode(col) {
+                Ok(data) => {
+                    println!("{}", data)
+                }
+                Err(_) => {}
+            }
+        }
+    }
+
+    let placeholders: Vec<NativeType> = vec![NativeType::Int32(1234)];
+    let rows_affected = con.execute_query_ph("delete from tweet where owner_id = ?", placeholders);
+
     con.disconnect()
 }
 
@@ -126,7 +168,7 @@ fn test_try_query_after_disconnect() {
     settings.password = "password".to_string();
     settings.database_name = "datajoint_core".to_string();
     settings.use_tls = Some(true);
-    settings.hostname = "localhost".to_string();
+    settings.hostname = "mysql_5_7".to_string();
     let mut con = Connection::new(settings);
 
     con.connect().unwrap();
@@ -150,7 +192,7 @@ fn test_postgres_query_to_mysql_db() {
     settings.password = "password".to_string();
     settings.database_name = "datajoint_core".to_string();
     settings.use_tls = Some(true);
-    settings.hostname = "localhost".to_string();
+    settings.hostname = "mysql_5_7".to_string();
     let mut con = Connection::new(settings);
 
     con.connect().unwrap();
@@ -163,6 +205,103 @@ fn test_postgres_query_to_mysql_db() {
 }
 
 #[test]
+fn test_executor() {
+    let mut settings = ConnectionSettings::new();
+    settings.database_type = DatabaseType::MySql;
+    settings.username = "root".to_string();
+    settings.port = 3306;
+    settings.password = "password".to_string();
+    settings.database_name = "datajoint_core".to_string();
+    settings.use_tls = Some(true);
+    settings.hostname = "mysql_5_7".to_string();
+    let mut con = Connection::new(settings);
+
+    con.connect().unwrap();
+
+    con.execute_query("truncate tweet");
+    let executor = con.executor();
+
+    // Execute a non-returning query.
+    let rows_affected: u64 =
+        executor.execute("insert into tweet (text, owner_id) values ('hello world', 1234);");
+    assert!(rows_affected == 1, "Rows affected did not equal 1.");
+
+    // Fetch a single row, ignoring the rest.
+    let single_row: TableRow = executor.fetch_one("select * from tweet;");
+    for col in single_row.columns() {
+        match single_row.try_decode(col) {
+            Ok(data) => {
+                println!("{}", data)
+            }
+            Err(_) => {}
+        }
+    }
+
+    // Testing executor with placeholders
+    let placeholders: Vec<NativeType> = vec![
+        NativeType::String("hello world".to_string()),
+        NativeType::Int32(1234),
+    ];
+    let rows_affected = executor.execute_ph(
+        "insert into tweet (text, owner_id) values (?, ?);",
+        placeholders,
+    );
+    assert!(rows_affected == 1, "Rows affected did not equal 1.");
+
+    let placeholders: Vec<NativeType> = vec![
+        NativeType::String("hello world".to_string()),
+        NativeType::Int32(1234),
+    ];
+    let cursor = &mut executor
+        .cursor_ph("select * from tweet where owner_id = ?", placeholders)
+        .unwrap();
+
+    let rows: Vec<TableRow> = cursor.rest();
+
+    for row in rows {
+        for col in row.columns() {
+            match row.try_decode(col) {
+                Ok(data) => {
+                    println!("{}", data)
+                }
+                Err(_) => {}
+            }
+        }
+    }
+
+    // Fetch all rows at once without a cursor.
+    let all_rows: Vec<TableRow> = executor.fetch_all("select * from tweet;");
+    for row in all_rows {
+        for col in row.columns() {
+            match row.try_decode(col) {
+                Ok(data) => {
+                    println!("{}", data)
+                }
+                Err(_) => {}
+            }
+        }
+    }
+
+    // Create a cursor for the given query.
+    let cursor = executor.cursor("select * from tweet;");
+    let cursor = &mut cursor.unwrap();
+
+    let rows: Vec<TableRow> = cursor.rest();
+
+    for row in rows {
+        for col in row.columns() {
+            match row.try_decode(col) {
+                Ok(data) => {
+                    println!("{}", data)
+                }
+                Err(_) => {}
+            }
+        }
+    }
+    con.disconnect();
+}
+
+#[test]
 fn run_test() {
     let mut settings = ConnectionSettings::new();
     settings.database_type = DatabaseType::MySql;
@@ -171,7 +310,7 @@ fn run_test() {
     settings.password = "password".to_string();
     settings.database_name = "datajoint_core".to_string();
     settings.use_tls = Some(true);
-    settings.hostname = "localhost".to_string();
+    settings.hostname = "mysql_5_7".to_string();
     let mut con = Connection::new(settings);
 
     con.connect().unwrap();
@@ -195,5 +334,5 @@ fn run_test() {
         }
     }
 
-    con.disconnect();
+    con.disconnect()
 }
