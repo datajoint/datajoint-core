26f8cb79f0fa90dc35f4ad8fb30bcbedeadb464b
benjuan-507 <bengjuan1234@gmail.com>
2022-02-20 16:51:00 -0600

    added dictionary pack

--- packages/datajoint-core/src/blob/blob.rs
+++ packages/datajoint-core/src/blob/blob.rs
@@ -1,55 +1,199 @@
 use std::convert::TryInto;
+use std::collections::HashMap;
+
+//[100, 106, 48, 0, 4, 5, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0, 10, 4, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0, 2, 4, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0, 20, 4, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0, 3, 5, 0, 0, 0, 0, 0, 0, 0, 10, 2, 0, 44, 1, 4, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0, 40, 4, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0, 5, 4, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0, 50]
+// def pack_dict(self, d):
+// return b"\4" + len_u64(d) + b"".join(
+//     b"".join((len_u64(it) + it) for it in packed)
+//     for packed in (map(self.pack_blob, pair) for pair in d.items()))
 
 fn main() {
-    let mut var: Vec<u8>= b"dj0\0".to_vec();
-    //var.append( &mut pack_int(5));
-    var.append(&mut pack_string("test"));
+    let test = HashMap::from([
+        (1,10),
+        (2,20),
+        (3,30),
+        (4,40),
+        (5,50),
+    ]);
 
-
-    //let bytes:Vec<u8> = vec![5, 0, 0, 0];
-    //let  unpackedvar = unpack_int(bytes);
-
+    let var = pack(test);
     println!("{:?}", var);
-    //println!("{}", unpackedvar);
+
+    //unpack(var);
 }
 
-fn pack_string(x: &str) -> Vec<u8>{
-    let mut packed: Vec<u8> = b"\x0a".to_vec();
+fn unpack (mut blob: Vec<u8>){
+    // Get Protocol
+    let pos = get_zero_terminated_pos(&blob);
+    blob.remove(pos);
+    let mut protocol: Vec<u8> = blob;
+    blob = protocol.split_off(pos);
+    //println!("Protocol: {:?} Blob: {:?}", protocol, blob);
 
-    let n_bytes = x.as_bytes().len();
-    packed.push(n_bytes.try_into().unwrap());
-    packed.push(b'\0');
-
-    packed.append(&mut x.as_bytes().to_vec()); // Data
-
-    return packed;
-      
-
-
-}
-// def pack_string(s):
-// blob = s.encode()
-// return b"\5" + len_u64(blob) + 
-
-
-fn pack_int(x: i32) -> Vec<u8>{
-    let mut packed: Vec<u8> = b"\x0a".to_vec(); // Prefix
-
-    let n_bytes = x.to_ne_bytes().len() as u8; // Size
-    packed.push(n_bytes);
-    packed.push(b'\0');
-
-    packed.append(&mut x.to_ne_bytes().to_vec()); // Data
-    
-    return packed;
+    read_blob(blob);
 }
 
-fn unpack_int(bytes:Vec<u8>) -> i32
-{
+fn read_blob(mut blob: Vec<u8>){
+    // Get Prefix
+    let prefix = blob.get(0).cloned().unwrap();
+    blob = blob.split_off(1);
+    //println!("Prefix: {} Blob: {:?}", prefix, blob);
 
-    let byte_arr = bytes.try_into().unwrap_or_else(|bytes: Vec<u8>|panic!("ERROR: Unable to convert to array"));
-    let num = i32::from_ne_bytes(byte_arr);
+    match prefix{
+        b'\x0a'=>println!("{}", unpack_int(blob)),
+        //b'\x0a'=>return unpack_int(blob),
+        //b'\x02'=>println!("{}", unpack_list(list)),
+        _=>println!("Not Implemented")
+    }
+}
 
-    return num;
+fn pack<T: Pack>(obj: T) -> Vec<u8> {
+    let mut blob: Vec<u8> = b"dj0\0".to_vec();
+    blob.append(&mut pack_blob(obj));
+    return blob;
+}
+
+#[inline]
+fn pack_blob<T: Pack>(obj: T) -> Vec<u8> {
+    let type_var = check_type(&obj);
+
+    let packed_data: Vec<u8> = {
+        match type_var {
+            "i32" => obj.as_int().pack(),
+            "i64" => obj.as_int().pack(),
+            _ => obj.pack(), // List, Dictionary
+            
+        }
+    };
+
+    return packed_data;
+}
+
+trait Pack {
+    //REMEMBER TO ADD YOUR FUNCTION HERE WHEN YOU WORK ON IT AND TO INCLUDE ALL
+    fn pack(&self) -> Vec<u8>;
+
+    fn as_string(self) -> String;
+    fn as_int(self) -> i64;
+}
+
+macro_rules! pack_list {
+    ($ty:ty) => {
+        impl Pack for Vec<$ty> {
+            fn pack(&self) -> Vec<u8> {
+                let mut packed: Vec<u8> = b"\x02".to_vec();
+                packed.push(self.len() as u8);
+                packed.push(b'\0');
+                
+                for n in 0..self.len() {
+                    packed.append( &mut pack_blob(*self.get(n).unwrap()));
+                }
+
+                return packed;
+            }
+
+            fn as_string(self) -> String {panic!()}
+            fn as_int(self) -> i64 {panic!()}
+        }
+    }
+}
+
+//INCLUDE IMPLEMENTATION HERE IF PRIMITIVE TYPE
+pack_list!(i64);
+
+macro_rules! pack_dictionary {
+    ($ty:ty) => {
+        impl Pack for HashMap<$ty, $ty> {
+            fn pack(&self) -> Vec<u8> {
+                let mut packed: Vec<u8> = b"\x04".to_vec();
+                
+                let num = self.len() as i64;
+                packed.append( &mut num.to_ne_bytes().to_vec());
+                
+                for (k,v) in self{
+                    packed.append(&mut len_u64(pack_blob(*k)));
+                    packed.append( &mut pack_blob(*k));
+
+                    packed.append(&mut len_u64(pack_blob(*v)));
+                    packed.append( &mut pack_blob(*v));
+                }
+
+                return packed;
+            }
+
+            fn as_string(self) -> String {panic!()}
+            fn as_int(self) -> i64 {panic!()}
+        }
+    }
+}
+
+//INCLUDE IMPLEMENTATION HERE IF PRIMITIVE TYPE
+pack_dictionary!(i64);
+
+
+impl Pack for i64 {
+    #[inline]
+    fn as_string(self) -> String {panic!()}
+    fn as_int(self) -> i64 {self}
+
+    fn pack(&self) -> Vec<u8> {
+        let mut packed: Vec<u8> = b"\x0a".to_vec(); // Prefix
+        let mut data: Vec<u8> = self.to_ne_bytes().to_vec(); // Data
+
+        // Get size
+        let mut n_bytes = self.to_ne_bytes().len(); // Size
+        while n_bytes > 0 {
+            if *data.get(n_bytes - 1).unwrap() == 0 {
+                n_bytes = n_bytes - 1;
+            }
+            else {
+                break;
+            }
+        }
+
+        // Get rid of extra 0's
+        data.truncate(n_bytes);
+
+        packed.push(n_bytes as u8);
+        packed.push(b'\0');
+
+        packed.append(&mut data); // Data
+        
+        return packed;
+    }
+}
+
+fn unpack_int(mut bytes:Vec<u8>) -> i64{
+    // Get n_byte
+    let pos = get_zero_terminated_pos(&bytes);
+    bytes.remove(pos);
+    let mut n_bytes: Vec<u8> = bytes;
+    bytes = n_bytes.split_off(pos);
+
+    let mut i = 8 - *n_bytes.get(0).unwrap();
+    while i > 0 {
+        bytes.push(b'\0');
+        i = i - 1;
+    }
+
+    let byte_arr = bytes.try_into().unwrap();
+    i64::from_ne_bytes(byte_arr)
+}
+
+fn check_type<T>(_obj: &T) -> &str {
+    let type_var: &str = std::any::type_name::<T>();
+    //println!("{}", std::any::type_name::<T>());
+
+    return type_var;
+}
+
+fn get_zero_terminated_pos (blob: &Vec<u8>) -> usize{
+    let mut iter = blob.iter();
+    return iter.position(|x| x == &b'\0').unwrap();
+}
+
+fn len_u64 (bytes: Vec<u8>) -> Vec<u8> {
+    let num = bytes.len() as i64;
+    num.to_ne_bytes().to_vec()
 }
 
