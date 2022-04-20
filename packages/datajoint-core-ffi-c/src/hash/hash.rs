8d080e8402841efa82e49cb14dbe78e53619f20e
Space-Cowboy2000 <78773289+Space-Cowboy2000@users.noreply.github.com>
2022-04-20 17:43:30 -0500

    Merge branch 'dev' of https://github.com/datajoint/datajoint-core into datajoint-dev

---
+++ packages/datajoint-core-ffi-c/src/hash/hash.rs
@@ -0,0 +1,21 @@
+
+
+#[no_mangle]
+pub unsafe extern "C" fn key_hash() -> {
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn uuid_from_stream() -> {
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn uuid_from_buffer() -> {
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn uuid_from_file() -> {
+
+}
