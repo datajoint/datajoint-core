31a4f56b2c5f8b2c8939b7b76497aafc0d11ff60
Space-Cowboy2000 <78773289+Space-Cowboy2000@users.noreply.github.com>
2022-04-21 15:42:30 -0500

    added cffi functions for Hash

--- packages/datajoint-core-ffi-c/src/hash/hash.rs
+++ packages/datajoint-core-ffi-c/src/hash/hash.rs
@@ -1,21 +1,27 @@
-
-
-// #[no_mangle]
-// pub unsafe extern "C" fn key_hash() -> {
-
-// }
+use datajoint_core::hash::{Hash};
+use std::ffi::CStr;
+use std::ffi::CString;
+use std::os::raw::c_char;
 
 // #[no_mangle]
-// pub unsafe extern "C" fn uuid_from_stream() -> {
-
+// pub unsafe extern "C" fn uuid_from_stream(stream: ) -> *const c_char{
+//     return "Hello"
 // }
 
-// #[no_mangle]
-// pub unsafe extern "C" fn uuid_from_buffer() -> {
+#[no_mangle]
+pub unsafe extern "C" fn uuid_from_buffer(bytes: *const c_char) -> *mut c_char{
+    let c_str = CStr::from_ptr(bytes);
+    let r_str = c_str.to_str().unwrap();
+    let answer = Hash::uuid_from_buffer(r_str.as_bytes());
+    let result = CString::new(answer).unwrap();
+    return result.into_raw();
+}
 
-// }
-
-// #[no_mangle]
-// pub unsafe extern "C" fn uuid_from_file() -> {
-
-// }
+#[no_mangle]
+pub unsafe extern "C" fn uuid_from_file(filepath: *const c_char) -> *mut c_char{
+    let c_str = CStr::from_ptr(filepath);
+    let r_str = c_str.to_str().unwrap();
+    let answer = Hash::uuid_from_file(r_str.to_string());
+    let result = CString::new(answer).unwrap();
+    return result.into_raw();
+}
